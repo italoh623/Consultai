82df449d04bc36c2b394cc559645b92f24a702a5
Lucas Grisi <lgoq@cin.ufpe.br>
2021-04-14 17:06:20 -0300

    remove db

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 const router = require('./router')
+const PORT = 3333
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.use('/api', router)
+app.use('/', router)
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
 
 module.exports = app
