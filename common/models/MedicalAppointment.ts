44ef4d7043c8c6bf06c9e9d46fab062dd6a035c8
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-05-03 00:49:44 -0300

    Adicionando diretório de arquivos comuns ao back e ao front

---
+++ common/models/MedicalAppointment.ts
@@ -0,0 +1,13 @@
+export class MedicalAppointment {
+  id: string
+  url: string
+  onCall: boolean
+  agendamentoId: string
+
+  constructor(id: string, url: string, onCall: boolean, agendamentoId: string) {
+    this.id = id;
+    this.url = url;
+    this.onCall = onCall;
+    this.agendamentoId = agendamentoId;
+  }
+}
