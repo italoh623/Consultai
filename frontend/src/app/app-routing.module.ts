57c8e8c44d879379a6b2aa49a11da50ece4e1a3b
Luisa Cavalcante <luisacavalcanteLuisa@MacBook-Air-de-Luisa.local>
2021-05-02 21:35:47 -0300

    Front Schedule

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppointmentComponent } from './appointment/appointment.component';
 import { RecordComponent } from './record/record.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 const routes: Routes = [
   { path: 'record/:id', component: RecordComponent },
-  { path: 'appointment', component: AppointmentComponent }
+  { path: 'appointment', component: AppointmentComponent },
+  { path: 'schedule', component: ScheduleComponent }
 ];
 
 @NgModule({
