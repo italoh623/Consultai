1090db9a76e7fa71a206680fa216435b9198ff40
Cadu Mello <ceblpm@cin.ufpe.br>
2021-05-03 13:55:35 -0300

    Scheduling list and service update

--- backend/src/routes/schedulingsRouter.ts
+++ backend/src/routes/schedulingsRouter.ts
@@ -18,6 +18,39 @@ schedulingsRouter.post('/', (request, response) => {
     return response.json(schedule)
 })
 
+schedulingsRouter.post('/month', (request, response) => {
+    const { crm, mes, ano } = request.body
+
+    const avaiable = schedulingService.filtrarDisponibilidadePorMes({
+        medicCRM: crm,
+        mes,
+        ano
+    })
+
+    return response.json(avaiable)
+})
+
+schedulingsRouter.post('/day', (request, response) => {
+    const { crm, dia, mes, ano } = request.body
+
+    const avaiable = schedulingService.filtrarDisponibilidadePorDia({
+        medicCRM: crm,
+        dia,
+        mes,
+        ano
+    })
+
+    return response.json(avaiable)
+})
+
+schedulingsRouter.get('/:especialidade', (request, response) => {
+    const { especialidade } = request.params
+
+    const schedule = schedulingService.filtrarPorEspecialidade(especialidade)
+
+    return response.json(schedule)
+})
+
 schedulingsRouter.delete('/:id', (request, response) => {
     const { id } = request.params
 
