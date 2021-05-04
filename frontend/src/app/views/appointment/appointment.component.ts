34007e6c78c0474b707555a7b05acfdbe64cbe58
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-05-03 21:03:52 -0300

    Configurando FormGroup ao AppointmentComponent

--- frontend/src/app/views/appointment/appointment.component.ts
+++ frontend/src/app/views/appointment/appointment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -8,15 +9,35 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class AppointmentComponent implements OnInit {
 
+  public appointmentForm: FormGroup;
+
   constructor(
-    public dialogRef: MatDialogRef<AppointmentComponent>
+    public dialogRef: MatDialogRef<AppointmentComponent>,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.appointmentForm = this.fb.group({
+      appointmentComplaints: [''],
+      appointmentPreviousDiseases: [''],
+      appointmentMedications: [''],
+      appointmentFamilyBackground: [''],
+      appointmentPhysicalExam: [''],
+      appointmentWeight: [''],
+      appointmentHeight: [''],
+      appointmentBloodPressure: [''],
+      appointmentComplementaryExams: [''],
+      appointmentDiagnosticHypothesis: [''],
+      appointmentConduct: [''],
+    });
   }
 
   cancel(): void {
     this.dialogRef.close();
+  }
+
+  sendForm(): void {
+    console.log(this.appointmentForm.value);
   }
 
 }
